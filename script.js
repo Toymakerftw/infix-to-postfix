6d90eb7a0c2a1e9a4d18479f0562c55ec890ec47
Anandhraman <37396330+Toymakerftw@users.noreply.github.com>
2023-04-22 17:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,23 @@ document.querySelector("form").addEventListener("submit", function(event) {
 
 	// Create a new paragraph element to display the postfix expression
 	const resultElem = document.createElement("p");
-	resultElem.textContent = postfix;
+
+	// Split the postfix expression into characters for animation
+	const postfixChars = postfix.split("");
+
+	// Add each character to the result element with animation
+	postfixChars.forEach(function(char, index) {
+		const charElem = document.createElement("span");
+		charElem.textContent = char;
+		charElem.style.opacity = 0;
+		charElem.style.transition = "opacity 0.5s ease-in-out " + (0.1 * index) + "s";
+		resultElem.appendChild(charElem);
+
+		// Trigger the animation by setting the opacity to 1 after a short delay
+		setTimeout(function() {
+			charElem.style.opacity = 1;
+		}, 10);
+	});
 
 	// Get the container element and append the result element
 	const containerElem = document.querySelector(".container");
@@ -32,3 +48,42 @@ function convertToPostfix(infix) {
 		"/": 2,
 		"+": 1,
 		"-": 1
+	};
+
+	for (let i = 0; i < infix.length; i++) {
+		const char = infix[i];
+
+		if (/[a-zA-Z0-9]/.test(char)) {
+			// If the character is a letter or number, add it to the postfix expression
+			postfix += char;
+		} else if (char in precedence) {
+			// If the character is an operator, add it to the stack
+			while (stack.length > 0 && stack[stack.length - 1] !== "(" && precedence[char] <= precedence[stack[stack.length - 1]]) {
+				postfix += stack.pop();
+			}
+			stack.push(char);
+		} else if (char === "(") {
+			// If the character is an opening parenthesis, add it to the stack
+			stack.push(char);
+		} else if (char === ")") {
+			// If the character is a closing parenthesis, pop operators from the stack and add them to the postfix expression until an opening parenthesis is found
+			while (stack.length > 0 && stack[stack.length - 1] !== "(") {
+				postfix += stack.pop();
+			}
+			if (stack[stack.length - 1] === "(") {
+				stack.pop();
+			}
+		}
+	}
+
+	// Pop any remaining operators from the stack and add them to the postfix expression
+	while (stack.length > 0) {
+		if (stack[stack.length - 1] !== "(" && stack[stack.length - 1] !== ")") {
+			postfix += stack.pop();
+		} else {
+			stack.pop();
+		}
+	}
+
+	return postfix;
+}
